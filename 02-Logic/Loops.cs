ec90905201b5517fbf0395ad1f4dca14e35c13f3
Teddy Paz <pazteddy@gmail.com>
2025-02-17 16:21:41 -0400

    Add additional loop examples and foreach implementation in Loops method

--- 02-Logic/Loops.cs
+++ 02-Logic/Loops.cs
@@ -6,15 +6,38 @@ partial class Program
     int counter = 1;
     while (counter <= 5)
     {
-      WriteLine($"Iteración: {counter}");
+      // WriteLine($"Iteración: {counter}");
       counter++;
     }
     //do while
     int number = 0;
     do
     {
-      WriteLine($"Número: {number}");
+      // WriteLine($"Número: {number}");
       number++;
     } while (number < 3);
+
+    for (int i = 0; i <= 5; i++)
+    {
+      WriteLine($"Iteración: {i}");
+    }
+    // Personalizando el for
+    for (int i = 10; i >= 0; i -= 2)
+    {
+      WriteLine(i);
+    }
+    //foreach
+    // Array
+    string[] fruits = ["Manzana", "Pera", "Piña"];
+    foreach (var fruit in fruits)
+    {
+      WriteLine(fruit);
+    }
+    // List
+    List<string> names = ["Pedro", "Luis", "Nancy"];
+    foreach (var name in names)
+    {
+      WriteLine(name);
+    }
   }
 }
