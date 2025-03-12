1fda1be5f35496cd5d521fcf5286e911730ffe08
Teddy Paz <pazteddy@gmail.com>
2025-03-12 19:17:25 -0400

    Enhance task display with BetterConsoleTables for improved formatting

--- 06-TaskMaster/Queries.cs
+++ 06-TaskMaster/Queries.cs
@@ -1,3 +1,6 @@
+using BetterConsoles.Tables;
+using BetterConsoles.Tables.Configuration;
+
 namespace TaskMaster
 {
   public class Queries(List<Task> _tasks)
@@ -8,12 +11,21 @@ namespace TaskMaster
     {
       ForegroundColor = ConsoleColor.DarkBlue;
       WriteLine("-----Lista de tareas-----");
+      // foreach (var task in Tasks)
+      // {
+      //   WriteLine("\n{0,-8} {1,-35} {2,-15}", "Id", "Descripción", "Completado");
+      //   WriteLine(new string('-', 58));
+      //   WriteLine("\n{0,-8} {1,-35} {2,-15}", task.Id, task.Description, task.Completed);
+      // }
+      Table table = new Table("Id", "Descripción", "Estado");
       foreach (var task in Tasks)
       {
-        WriteLine("\n{0,-8} {1,-35} {2,-15}", "Id", "Descripción", "Completado");
-        WriteLine(new string('-', 58));
-        WriteLine("\n{0,-8} {1,-35} {2,-15}", task.Id, task.Description, task.Completed);
+        table.AddRow(task.Id, task.Description, task.Completed ? "Completada" : "");
       }
+      table.Config = TableConfig.Unicode();
+
+      Write(table.ToString());
+      ReadKey();
     }
 
   }
